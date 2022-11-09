58fb310374ac8a435a9c226e6ef856d5d09ac11b
Trung Võ <chinhtrunguit@gmail.com>
2022-11-09 16:08:55 +0700

    3 Components and Visual Effect

---
+++ Courses/Courses/Extensions/Color+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Extensions.swift
+//  Courses
+//
+//  Created by TrungVo_Diag on 09/11/2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let accent = Color("accent")
+    static let card4 = Color("card4")
+    static let card3 = Color("card3")
+}
---
+++ Courses/Courses/Extensions/Image+Exensions.swift
@@ -0,0 +1,14 @@
+//
+//  Image+Exensions.swift
+//  Courses
+//
+//  Created by TrungVo_Diag on 09/11/2022.
+//
+
+import SwiftUI
+
+extension Image {
+    static let logo1 = Image("Logo1")
+    static let card1 = Image("Card1")
+    static let background1 = Image("Background1")
+}
